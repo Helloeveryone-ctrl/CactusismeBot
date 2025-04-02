1e44f9612c1bcd73da9694d095d058e2d1c64a5c
Helloeveryone-ctrl <reefcoral810@gmail.com>
2025-04-02 09:41:16 +0800

    Create user-config.py

---
+++ user-config.py
@@ -0,0 +1,13 @@
+# Pywikibot User Configuration
+usernames = {
+    'wikiversity': {
+        'hi': 'CactusismeBot'  # Your bot’s username for Hindi Wikiversity
+    }
+}
+
+# Targeting Hindi Wikiversity
+family = 'wikiversity'  # Wiki family
+mylang = 'hi'  # Hindi language code
+
+# Login method using password file
+password_file = 'user-password.py'
